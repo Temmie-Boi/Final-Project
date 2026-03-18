a6bb7d96464b7e7a301cfd3a0f0db738588338bf
Lorenzo Duran <Lduran0127@conestogac.on.ca>
2026-03-18 13:18:38 -0400

    Changed a tiny bit of the file architecture

---
+++ scripts/Taskmanual.js
@@ -0,0 +1,32 @@
+const wikihowMap = {
+		bathroom: "https://www.wikihow.com/Clean-a-Bathroom",
+		bedroom: "https://www.wikihow.com/Clean-Your-Room",
+		bed: "https://www.wikihow.com/Make-Your-Bed",
+		dishwash: "https://www.wikihow.com/Wash-Dishes"
+		};
+		
+		const selectatask = document.getElementById("taskInstructMenu");
+		
+		selectatask.addEventListener("change", () => {
+		const manualpage = selectatask.value;
+		if (!manualpage) return;
+		const url = wikihowMap[manualpage];
+		
+		window.open(url, "_blank");
+		});
+		
+        btnMain.addEventListener("click", () => {
+                window.location.href = "Mainpage.html";
+            })
+		btnCalendar.addEventListener("click", () => {
+				window.location.href = "Calendar.html";
+			})
+		btnCurrentTask.addEventListener("click", () => {
+				window.location.href = "Tasks.html";
+			})
+		btnAddDeleteTask.addEventListener("click", () => {
+				window.location.href = "AddorDeleteTask.html";
+			})
+		btnLearnAboutUs.addEventListener("click", () => {
+				window.location.href = "Learnaboutus.html";
+			})
