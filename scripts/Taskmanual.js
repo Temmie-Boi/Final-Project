f60bd6a13ab6f8d7a31859b92afae1a6c8ea541d
Evan Lucero <elucero2371@conestogac.on.ca>
2026-04-14 13:22:33 -0400

    Add text to Learn about us, and commented code work in progress for task manual with supabase

--- scripts/Taskmanual.js
+++ scripts/Taskmanual.js
@@ -1,9 +1,32 @@
-const wikihowMap = {
-		bathroom: "https://www.wikihow.com/Clean-a-Bathroom",
-		bedroom: "https://www.wikihow.com/Clean-Your-Room",
-		bed: "https://www.wikihow.com/Make-Your-Bed",
-		dishwash: "https://www.wikihow.com/Wash-Dishes"
-		};
+const SUPABASE_URL = "https://vdwkkqbosjnftksgppuj.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_fbO9zQaPZQfhEJfVOOsUXw_L9fRzHI_";
+
+//const supabaseClient = supabase.creatClient(
+//	SUPABASE_URL,
+//	SUPABASE_PUBLISHABLE_KEY
+//);
+
+//const form = document.getElementById("tasklinkform")
+
+//form.addEventListener("submit", async function (e) {
+
+//	e.preventDefault();
+
+//const bathroom = document.getElementById("bathroom").window.open();
+//const bedroom = document.getElementById("bedroom").window.open();
+//const bed = document.getElementById("bed").window.open();
+//const dishwash = document.getElementById("dishwash").window.open();
+
+//const result = await subabaseClient
+//.from("the_task_manual")
+
+//if (result.error) {
+//	alert("ERROR loading website!");
+//	return;
+//}
+
+//});
+
 document.addEventListener("DOMContentLoaded", function() {
 		
     	const btnMain = document.getElementById("btnMain"); //Constants for the buttons on every page
@@ -13,11 +36,19 @@ document.addEventListener("DOMContentLoaded", function() {
     	const btnLearnAboutUs = document.getElementById("btnLearnAboutUs");			
 		const selectatask = document.getElementById("taskSelection");
 		
+		const wikihowMap = {
+		bathroom: "https://www.wikihow.com/Clean-a-Bathroom",
+		bedroom: "https://www.wikihow.com/Clean-Your-Room",
+		bed: "https://www.wikihow.com/Make-Your-Bed",
+		dishwash: "https://www.wikihow.com/Wash-Dishes"
+		};
+
 		selectatask.addEventListener("change", () => {
-		const manualpage = selectatask.value;
+		const manualpage = selectatask.value; 
 		if (!manualpage) return;
 		const url = wikihowMap[manualpage];
-		
+
+
 		window.open(url, "_blank");
 		});
 		
@@ -37,4 +68,4 @@ document.addEventListener("DOMContentLoaded", function() {
 		btnLearnAboutUs.addEventListener("click", () => {
 				window.location.href = "Learnaboutus.html";
 			})
-		});
+});
