a99d7d5455b8ee3d844bd9656f3fb3eed67eb89b
Michelle_Watkins <mpwatkins@rogers.com>
2026-04-14 20:56:00 -0400

    Taskmanager pulls from Supabase with same opening new window, with change to css to make it look a little better

--- scripts/Taskmanual.js
+++ scripts/Taskmanual.js
@@ -1,32 +1,47 @@
 const SUPABASE_URL = "https://vdwkkqbosjnftksgppuj.supabase.co";
 const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_fbO9zQaPZQfhEJfVOOsUXw_L9fRzHI_";
 
-//const supabaseClient = supabase.creatClient(
-//	SUPABASE_URL,
-//	SUPABASE_PUBLISHABLE_KEY
-//);
+const supabaseClient = supabase.createClient(
+	SUPABASE_URL,
+	SUPABASE_PUBLISHABLE_KEY
+);
 
-//const form = document.getElementById("tasklinkform")
+const form = document.getElementById("tasklinkform")
 
-//form.addEventListener("submit", async function (e) {
+form.addEventListener("change", async function (e) { //not a submit, a change because no submit button
+	//console.log("change fired"); confirmed it worked
+	
+	//e.preventDefault(); Not needed
 
-//	e.preventDefault();
-
+	//TODO: supabase data here, below is legacy code we tried
 //const bathroom = document.getElementById("bathroom").window.open();
 //const bedroom = document.getElementById("bedroom").window.open();
 //const bed = document.getElementById("bed").window.open();
 //const dishwash = document.getElementById("dishwash").window.open();
 
-//const result = await subabaseClient
-//.from("the_task_manual")
+const selectedTask = e.target.value //actually chooses what the user selected
 
-//if (result.error) {
-//	alert("ERROR loading website!");
-//	return;
-//}
+	//console.log(selectedTask); confirms values match
 
-//});
+const {data, error} = await supabaseClient //pulls data and error pieces right away
+	.from("the_task_manual")
+	.select("*") //We need to actually select the data
+	.eq("name", selectedTask); //filter to the correct row
 
+	//console.log(data); //Make sure the supabase is sending info
+	//console.log(error);
+if (error) {
+	//console.log(data); //Code finding supabase errors
+	alert("ERROR loading website!");
+	return;
+}
+
+window.open(data[0].Links, "_blank");
+//console.log(data); //check return record
+
+});
+
+//Old Vanilla Javascript Code
 document.addEventListener("DOMContentLoaded", function() {
 		
     	const btnMain = document.getElementById("btnMain"); //Constants for the buttons on every page
@@ -36,20 +51,20 @@ document.addEventListener("DOMContentLoaded", function() {
     	const btnLearnAboutUs = document.getElementById("btnLearnAboutUs");			
 		const selectatask = document.getElementById("taskSelection");
 		
-		const wikihowMap = {
-		bathroom: "https://www.wikihow.com/Clean-a-Bathroom",
-		bedroom: "https://www.wikihow.com/Clean-Your-Room",
-		bed: "https://www.wikihow.com/Make-Your-Bed",
-		dishwash: "https://www.wikihow.com/Wash-Dishes"
-		};
+		//const wikihowMap = {
+		//bathroom: "https://www.wikihow.com/Clean-a-Bathroom",
+		//bedroom: "https://www.wikihow.com/Clean-Your-Room",
+		//bed: "https://www.wikihow.com/Make-Your-Bed",
+		//dishwash: "https://www.wikihow.com/Wash-Dishes"
+		//};
 
-		selectatask.addEventListener("change", () => {
-		const manualpage = selectatask.value; 
-		if (!manualpage) return;
-		const url = wikihowMap[manualpage];
+		//selectatask.addEventListener("change", () => {
+		//const manualpage = selectatask.value; 
+		//if (!manualpage) return;
+		//const url = wikihowMap[manualpage];
 
 
-		window.open(url, "_blank");
+		//window.open(url, "_blank");
 		});
 		
 	
@@ -68,4 +83,5 @@ document.addEventListener("DOMContentLoaded", function() {
 		btnLearnAboutUs.addEventListener("click", () => {
 				window.location.href = "Learnaboutus.html";
 			})
-});
+//})
+;
